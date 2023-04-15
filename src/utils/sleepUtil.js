b041e65151ad80c40b7b689d52678a7f630f1148
Tasha Pais <tashapais@gmail.com>
2023-04-14 21:36:18 -0400

    solana-game

--- src/utils/sleepUtil.js
+++
@@ -1,3 +0,0 @@
-export const sleepUtil = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
--- src/utils/walletUtils.js
+++
@@ -1,6 +0,0 @@
-import {
-    Connection
-  } from "@solana/web3.js";
-import { NETWORK } from "./nftCreation";
-
-export const connection = new Connection(NETWORK);
